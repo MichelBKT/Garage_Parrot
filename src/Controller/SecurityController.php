65bc3782f618491daa671b1525ba980fc480e40c
Michel Banckaert <banckaertm@hotmail.com>
2023-11-21 16:55:26 +0100

    sitemap + dashboard users

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -13,9 +13,9 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils, TimetableRepository $timetableRepository): Response
     {
-         if ($this->getUser()) {
-             return $this->redirectToRoute('admin');
-         }
+        // if ($this->getUser()) {
+         //    return $this->redirectToRoute('admin');
+        // }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
