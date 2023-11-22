af5227f8fa02b1121b04966dcc92fa44e2ae7eae
Michel Banckaert <banckaertm@hotmail.com>
2023-11-22 19:14:25 +0100

    db modif2

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php
-
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
@@ -7,3 +6,21 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+
+// sur Heroku
+if (getenv('JAWSDB_URL') !== false) {
+
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
+    
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'],'/');
+} else {
+    // en local
+    $username = 'root';
+    $password = '';
+    $database = "parrot_garage";
+    $hostname = 'localhost';
+}
