15d27f51828661a1b523673df326db484b2a09b0
Michel Banckaert <banckaertm@hotmail.com>
2023-11-22 19:11:39 +0100

    db modif

--- jawsdb.php
+++ jawsdb.php
@@ -1,9 +1,19 @@
 <?php
+// sur Heroku
+if (getenv('JAWSDB_URL') !== false) {
 
-$url = getenv('JAWSDB_URL');
-$dbparts = parse_url($url);
-
-$hostname = $dbparts['host'];
-$username = $dbparts['user'];
-$password = $dbparts['pass'];
-$database = ltrim($dbparts['path'],'/');
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
+    
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'],'/');
+} else {
+    // en local
+    $username = 'root';
+    $password = '';
+    $database = "parrot_garage";
+    $hostname = 'localhost';
+}
+    
