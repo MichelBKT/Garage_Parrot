b0c2f2993849044dba185a8703dd505837787e4f
Michel Banckaert <banckaertm@hotmail.com>
2023-11-22 18:54:48 +0100

    db

---
+++ jawsdb.php
@@ -0,0 +1,9 @@
+<?php
+
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
